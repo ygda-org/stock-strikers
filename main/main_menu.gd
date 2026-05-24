21825b498739681e313328c0237b0cd05f81a208
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-05-24 12:53:55 -0400

    credits

---
+++ main/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("uid://dqak3awcpfb8w")
+
+
+func _on_credits_pressed():
+	$Credits.visible = true
+	$Main.visible = false
+	
+
+
+
+func _on_back_pressed():
+	$Credits.visible = false
+	$Main.visible = true
--- main/startup_scene.gd
+++ main/startup_scene.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _on_animation_player_animation_finished(anim_name):
-	get_tree().change_scene_to_file("uid://dqak3awcpfb8w")
+	get_tree().change_scene_to_file("uid://6i6mv001enok")
