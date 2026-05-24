6fff4a3f2f5648d3a1ecd24cd8a92d1714a8393b
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-05-24 12:48:11 -0400

    fix

--- main/startup_scene.gd
+++ main/startup_scene.gd
@@ -1,9 +1,4 @@
 extends Node2D
 
-
-func _on_animated_sprite_2d_animation_finished():
-	$AnimationPlayer.play("fade_out")
-
-
 func _on_animation_player_animation_finished(anim_name):
 	get_tree().change_scene_to_file("uid://dqak3awcpfb8w")
