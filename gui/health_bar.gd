d9f5fa65cf00f0819e51bae368fa4535e3d7bbe0
MrPuffyWuff <jasper13.com@gmail.com>
2026-05-28 23:43:04 -0400

    health bar simplification and new look

--- gui/health_bar.gd
+++ gui/health_bar.gd
@@ -1,11 +1,10 @@
 extends Control
-@export var bar:ProgressBar
-@export var text:Label
-var connected:bool = false
+
+var connected : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -14,6 +13,5 @@ func _process(delta: float) -> void:
 		connected = true
 
 func _on_player_hp_update(max_hp,current_hp):
-	bar.max_value = max_hp
-	bar.value = current_hp
-	text.text = str(current_hp) + "/" + str(max_hp)
+	$Bar.max_value = max_hp
+	$Bar.value = current_hp
