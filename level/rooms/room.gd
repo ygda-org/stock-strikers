775654b490b36d51d8f39d63d33db9ab9917f704
MrPuffyWuff <jasper13.com@gmail.com>
2026-06-02 21:51:20 -0400

    dsadas

--- level/rooms/room.gd
+++ level/rooms/room.gd
@@ -13,6 +13,10 @@ class_name Room
 
 @onready var center : Vector2 = position + Vector2(128,128)
 
+func _ready() -> void:
+	for poz in spawns:
+		pass
+
 func _process(delta: float) -> void:
 	var sq_dist_to_player := (GameState.player.position - center).length_squared()
 	
