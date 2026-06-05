d10ecb07692a194697d1abc9941b297fa651d52e
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-06-04 22:38:03 -0400

    moosic

--- main/main.gd
+++ main/main.gd
@@ -7,5 +7,6 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if GameState.enemies.is_empty():
+		$AudioStreamPlayer.bus = &"Low"
