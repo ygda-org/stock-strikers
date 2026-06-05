19a577decfb1522e5fdb9ceb8b66567c9c936b7d
MrPuffyWuff <jasper13.com@gmail.com>
2026-06-05 17:07:04 -0400

    elevator open is here

--- level/rooms/elevator.gd
+++ level/rooms/elevator.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+var open := false
+
+func _process(delta: float) -> void:
+	if not open and GameState.enemies.is_empty():
+		$AnimatedSprite2D.play("open")
+		open = true
 
 func _on_body_entered(body):
 	if body.is_in_group("Player") and GameState.enemies.is_empty():
