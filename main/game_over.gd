427bf5c3cf2072dfed5ac0ec9b3d283e91659d21
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-06-07 22:07:26 -0400

    game over screen

---
+++ main/game_over.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	$Label.text = "Highest floor: " + str(GameState.cleared_floors)
+
+func _on_button_pressed():
+	$Button.disabled = true
+	$AnimationPlayer.play("fade_out")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "fade_out":
+		get_tree().change_scene_to_file("uid://6i6mv001enok")
