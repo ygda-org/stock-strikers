dde2356411c604f1a2384ec8b770e4e6f8dba4c7
MrPuffyWuff <jasper13.com@gmail.com>
2026-06-07 22:26:30 -0400

    misc

--- main/game_over.gd
+++ main/game_over.gd
@@ -1,4 +1,6 @@
-extends Node2D
+extends Control
+
+var is_replay := false
 
 func _ready():
 	$Label.text = "Highest floor: " + str(GameState.cleared_floors)
@@ -11,4 +13,13 @@ func _on_button_pressed():
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fade_out":
 		PlayerStats.reset_stats()
-		get_tree().change_scene_to_file("uid://6i6mv001enok")
+		if is_replay:
+			get_tree().change_scene_to_file("uid://dqak3awcpfb8w")
+		else:
+			get_tree().change_scene_to_file("uid://6i6mv001enok")
+
+
+func _on_button_2_pressed() -> void:
+	is_replay = true
+	$Button2.disabled = true
+	$AnimationPlayer.play("fade_out")
