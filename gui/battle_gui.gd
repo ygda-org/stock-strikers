d6d8e1b23125df25a0ca58289807e31f3e32ada5
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-06-07 21:34:49 -0400

    fixes

--- gui/battle_gui.gd
+++ gui/battle_gui.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 func _process(_delta):
 	$Panel/VBoxContainer/Money.text = "$" + str(snapped(PlayerStats.money,0.01))
 	$Panel/VBoxContainer/Debt.text = "$" + str(PlayerStats.get_total_debt())
+	$Panel/VBoxContainer/Floor.text = "Floor " + str(GameState.cleared_floors)
 
 func on_update_stock_tickers():
 	var half : int = int(PlayerStats.stocks.size()/2.0)
